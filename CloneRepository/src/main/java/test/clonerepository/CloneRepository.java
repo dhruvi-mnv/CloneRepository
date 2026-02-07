4ffead470aae946545c0b9d5f9236a5615e9ae17
dhruvi-mnv <jariwadh@sheridancollege.ca>
2026-02-06 22:02:27 -0500

    FOURTH COMMIT new code added to feature/clone-branch

---
+++ CloneRepository/src/main/java/test/clonerepository/CloneRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.clonerepository;
+
+/**
+ *
+ * @author dhruv
+ */
+public class CloneRepository {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public double multiply(double a, double b){
+        return a*b;
+    }
+}
